c4d46cb6e9b4ec0969eb062cf5511f2f4b1c34ec
Mike Mochan <mmochan@mac.com>
2015-08-31 09:25:45 +1000

    clean up

--- gpx.rb
+++ gpx.rb
@@ -3,53 +3,44 @@
 require 'nokogiri'
 
 class Gpx
-  attr_accessor :gpx_names
-  attr_reader :file, :doc
+  attr_accessor :gpx_name_nodes, :saved, :count
 
   def initialize  file
-    @file = file
     @doc = Nokogiri::XML(file)
-    @gpx_names = @doc.xpath('//gpx:name', 'gpx' => 'http://www.topografix.com/GPX/1/1')
+    @gpx_name_nodes = @doc.xpath('//gpx:name', 'gpx' => 'http://www.topografix.com/GPX/1/1')
     @saved = []
     @count = 0
   end
 
   def parse_name
     puts "Parsing multi track GPX xml data"
-    gpx_names.each do |name|
-      old_position = save_position name
-      if name.content.end_with?("Position 1")
+    gpx_name_nodes.each do |node|
+      old_position = save_position node
+      if node.content.end_with?("Position 1")
         @count += 1
-        @index = @saved.rindex(old_position)
-        increment_until_next_position_one(@index, name)
+        @index = saved.rindex(old_position)
+        increment_until_next_position_one(@index, node)
       end
       if @count > 1
-        increment_until_next_position_one(@index, name)
+        increment_until_next_position_one(@index, node)
         @index += 1
       end
-
     end
     write_new_file
-    close_file
   end
 
-  def increment_until_next_position_one(index, name)
-    name.content = "Position " + (index + 1).to_s unless index == 0
+  def increment_until_next_position_one(index, node)
+    node.content = "Position " + (index + 1).to_s unless index == 0
   end
 
-  def save_position name 
-    @saved << name.content 
-    @saved[-2]
+  def save_position node 
+    saved << node.content 
+    saved[-2]
   end
 
   def write_new_file
     puts "Writing new file"
     File.write("./trip_total.gpx", @doc.to_xml)
-  end
-
-  def close_file
-   puts "Closing file"
-   @file.close 
   end
 end
 
